b37bc8bf82895fad6083b33be689ebad524ccac1
pin1ker <114922537+pin1ker@users.noreply.github.com>
2022-11-14 21:38:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+"use stick"
+
+let userName = prompt('Как вас зовут?' , 0);
+
+let userAge = prompt('Сколько вам лет?' , 0);
+
+let userMood = prompt('Какое у вас настроние?' , 0);
+alert(`Вас зовут ${userMood}\nВам ${userAge} \nВаше ${userMood} настроние мне нравится!  `)
