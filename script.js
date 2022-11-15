e04a463d09346238e3a2ca99752b9469fcfee6fe
pin1ker <114922537+pin1ker@users.noreply.github.com>
2022-11-15 07:56:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ let userName = prompt('Как вас зовут?' , 0);
 let userAge = prompt('Сколько вам лет?' , 0);
 
 let userMood = prompt('Какое у вас настроние?' , 0);
-alert(`Вас зовут ${user}\nВам ${userAge} \nВаше ${userMood} настроние мне нравится!  `)
+alert(`Вас зовут ${userName}\nВам ${userAge} \nВаше ${userMood} настроние мне нравится!  `)
