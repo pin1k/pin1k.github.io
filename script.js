04a0829e2039115536e6ed097a26c4c81177e61e
pin1ker <114922537+pin1ker@users.noreply.github.com>
2022-11-27 08:54:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 "use strict"
 let a = prompt('Сколько коров на лугу?(Писать только цифрами)and(0=<korov=<100)',12);
 let c = a % 10
-
 if (a <= 14 && a >= 11){
-  alert(`${a} korov`);
+  alert(`${a} коров`);
 } else if (c == 0 || c >=5 && c<=9){
-  alert(`${a} korov`)
+  alert(`${a} коров`)
 } else if(c == 1) {
-  alert(`${a} korova`);
+  alert(`${a} корова`);
 } else if(c >=2 && c <=4){
-  alert(`${a} korovy`)
+  alert(`${a} коровы`)
 }
   
